1866df833abffd8346a7a7745dbce1aa4e7b5305
jweaker <jweaker.t@gmail.com>
2023-09-08 16:22:54 +0300

    feat: flash,noice,neoscroll etc.

--- config.lua
+++ config.lua
@@ -11,11 +11,12 @@ local actions = require("telescope.actions")
 vim.opt.relativenumber = true
 if vim.g.neovide then
 	vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+	vim.g.neovide_hide_mouse_when_typing = true
 end
 
 lvim.format_on_save = true
 lvim.transparent_window = false
-lvim.colorscheme = "catppuccin"
+lvim.colorscheme = "onedark"
 lvim.keys.normal_mode["<Esc>"] = ":noh <CR>"
 lvim.keys.normal_mode["<Tab>"] = "<cmd>BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-Tab>"] = "<cmd>BufferLineCyclePrev<CR>"
@@ -98,6 +99,13 @@ vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyz
 
 lvim.plugins = {
 	{
+		"karb94/neoscroll.nvim",
+		lazy = false,
+		config = function()
+			require("neoscroll").setup()
+		end,
+	},
+	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
@@ -152,15 +160,15 @@ lvim.plugins = {
 			})
 		end,
 	},
-	-- {
-	-- 	"navarasu/onedark.nvim",
-	-- 	config = function()
-	-- 		require("onedark").setup({
-	-- 			style = "warmer",
-	-- 		})
-	-- 		require("onedark").load()
-	-- 	end,
-	-- },
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").setup({
+				style = "warmer",
+			})
+			require("onedark").load()
+		end,
+	},
 	{
 		"folke/flash.nvim",
 		event = "VeryLazy",
@@ -213,9 +221,9 @@ lvim.plugins = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
 			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
+			-- `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
+			-- "rcarriga/nvim-notify",
 		},
 	},
 	{
@@ -243,15 +251,15 @@ lvim.plugins = {
 		dependencies = "hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 	},
-	-- {
-	-- 	"ObserverOfTime/nvimcord",
-	-- 	lazy = false,
-	-- 	config = function()
-	-- 		require("nvimcord").setup({
-	-- 			autostart = true,
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"ObserverOfTime/nvimcord",
+		lazy = false,
+		config = function()
+			require("nvimcord").setup({
+				autostart = true,
+			})
+		end,
+	},
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		lazy = false,
@@ -260,7 +268,6 @@ lvim.plugins = {
 				ensure_installed = {
 					"typescript-language-server",
 					"jsonls",
-					"rust",
 					"lua_ls",
 					"emmet_ls",
 					"bash-language-server",
@@ -274,36 +281,6 @@ lvim.plugins = {
 				run_on_start = true,
 				start_delay = 3000, -- 3 second delay
 				debounce_hours = 5, -- at least 5 hours between attempts to install/update
-			})
-		end,
-	},
-	{
-		"andweeb/presence.nvim",
-		event = "VeryLazy",
-		config = function()
-			-- The setup config table shows all available config options with their default values:
-			require("presence").setup({
-				-- General options
-				auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-				neovim_image_text = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
-				main_image = "neovim", -- Main image display (either "neovim" or "file")
-				client_id = "793271441293967371", -- Use your own Discord application client id (not recommended)
-				log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-				debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
-				enable_line_number = false, -- Displays the current line number instead of the current project
-				blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-				buttons = false, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
-				file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-				show_time = true, -- Show the timer
-
-				-- Rich Presence text options
-				editing_text = "Editing %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-				file_explorer_text = "Browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-				git_commit_text = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-				plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-				reading_text = "Reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-				workspace_text = "Working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-				line_number_text = "Line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
 			})
 		end,
 	},
